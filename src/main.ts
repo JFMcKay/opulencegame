a4d9c6c3c92315ca9b1883ffc4aa17f83204bcfe
Jeremy F McKay <jeremyfmckay@gmail.com>
2023-04-03 11:33:37 -0700

    Rewrote the script using typescript

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,35 @@
-"use strict";
 // Get the modal element
-let modal = document.getElementById("signup-modal");
+let modal: HTMLElement = document.getElementById("signup-modal")!;
 // Get the button/link that opens the modal
-let button = document.getElementById("signup-button");
+let button: HTMLElement = document.getElementById("signup-button")!;
 // Get the <span> element that closes the modal
-let span = modal.querySelector(".close");
+let span: HTMLElement = modal.querySelector(".close") as HTMLElement;
 // Get the card element that flips the card
-let card = document.getElementById('card');
-function flipCard() {
+let card: HTMLElement = document.getElementById('card')!;
+
+function flipCard(): void {
     if (modal.style.display === 'block') {
         return;
     }
     card.classList.toggle('flipped');
 }
+
 card.addEventListener('click', () => flipCard());
+
 // When the user clicks the button, open the modal
-button.onclick = function () {
+button.onclick = function(): void {
     modal.style.display = "block";
-};
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.onclick = function(): void {
     modal.style.display = "none";
-};
+}
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event: MouseEvent): void {
     event.stopPropagation();
     if (event.target === modal) {
         modal.style.display = "none";
     }
-};
+}
